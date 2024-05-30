82d10cba9c3d21b56275866bb7abe5e96b8e90e4
MiguelPim01 <miguelmip02@gmail.com>
2024-05-30 16:13:55 -0300

    build: Inicial commit

---
+++ src/main/java/com/example/PathToGrade/PathToGradeApplication.java
@@ -0,0 +1,13 @@
+package com.example.PathToGrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathToGradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathToGradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PathToGrade/PathToGradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PathToGrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathToGradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
