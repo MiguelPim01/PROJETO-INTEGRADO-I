fb8d496d3223c5e412849ff4c37a58460dae211a
MiguelPim01 <miguelmip02@gmail.com>
2024-06-22 18:37:12 -0300

    docs: Documentação da classe CursoRepository adicionada

--- src/main/java/com/example/PathToGrade/repository/CursoRepository.java
+++ src/main/java/com/example/PathToGrade/repository/CursoRepository.java
@@ -4,6 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.PathToGrade.domain.Curso;
 
+/**
+ * <i>Documentação da classe CursoRepository</i>
+ * @author Miguel Vieira Machado Pim
+ * @see com.example.PathToGrade.repository.CursoRepository
+ * 
+ * Repositório CRUD para armazenar objetos da classe Curso.
+ */
 public interface CursoRepository extends CrudRepository<Curso, Long> {
     
 }
