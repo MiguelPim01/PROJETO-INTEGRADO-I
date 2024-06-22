ef060d9b6c4add5b604a15b4eef5393e685e30b4
MiguelPim01 <miguelmip02@gmail.com>
2024-06-22 18:32:43 -0300

    docs: Documentação da controladora adicionada

--- src/main/java/com/example/PathToGrade/controller/CursoController.java
+++ src/main/java/com/example/PathToGrade/controller/CursoController.java
@@ -18,80 +18,169 @@ import com.example.PathToGrade.domain.Dependencia;
 import com.example.PathToGrade.domain.Disciplina;
 import com.example.PathToGrade.service.CursoService;
 
+/**
+ * <i>Documentação da classe CursoController</i>
+ * @author Miguel Vieira Machado Pim
+ * @see com.example.PathToGrade.controller.CursoController
+ * 
+ * Uma RestController para prover as operações HTTP necessárias para o funcionamento da API.
+ * <p>
+ * Os endpoints providos por essa controladora são:
+ * <ul>
+ * <li> GET "/curso"
+ * <li> GET "/curso/{cursoId}/disciplina"
+ * <li> GET "/curso/{cursoId}/path/{disciplinaId}"
+ * <li> POST "/curso"
+ * <li> POST "/curso/{cursoId}/disciplina"
+ * <li> POST "/curso/{cursoId}/disciplinas"
+ * <li> POST "/curso/{cursoId}/preRequisito"
+ * <li> POST "/curso/{cursoId}/preRequisitos"
+ * <li> PUT "/curso/{cursoId}"
+ * <li> PUT "/curso/{cursoId}/disciplina/{disciplinaId}"
+ * <li> DELETE "/curso/{cursoId}"
+ * <li> DELETE "/curso/{cursoId}/disciplina/{disciplinaId}"
+ * </ul>
+ * Esses endpoints permitem a inclusão de cursos, disciplinas e pré-requisitos no banco de dados, assim como a deleção e modificação deles.
+ * 
+ * @version %I%, %G%
+ */
 @RestController
 public class CursoController {
     
+    /**
+     * Variável para prover as funcionalidades de cursoService para a controladora.
+     */
     @Autowired
     private CursoService cursoService;
 
-    /*
-     *  Operações HTTP para serem feitas com Curso
+    /**
+     * Retorna todos os cursos do banco de dados.
+     * 
+     * @return Lista de Cursos.
      */
     @GetMapping("/curso")
     public Iterable<Curso> getCursos() {
         return cursoService.getCursos();
     }
 
+    /**
+     * Insere um Curso no banco de dados.
+     * 
+     * @param curso Curso passado no json.
+     */
     @PostMapping("/curso")
     public void postCurso(@RequestBody Curso curso) {
         cursoService.saveCurso(curso);
     }
 
+    /**
+     * Modifica um Curso no banco de dados.
+     * 
+     * @param cursoId id do Curso.
+     * @param curso novo Curso contendo as informações a serem modificadas.
+     */
     @PutMapping("/curso/{cursoId}")
     public void putCurso(@PathVariable("cursoId") Long cursoId, @RequestBody Curso curso) {
         cursoService.modifyCurso(cursoId, curso);
     }
 
+    /**
+     * Deleta um Curso do banco de dados.
+     * 
+     * @param cursoId id do Curso.
+     */
     @DeleteMapping("/curso/{cursoId}")
     public void deleteCurso(@PathVariable(name = "cursoId") Long cursoId) {
         cursoService.deleteCurso(cursoId);
     }
 
-    /*
-     *  Operações HTTP para serem feitas com Disciplina
+    /**
+     * Retorna todas as disciplinas de um Curso específico.
+     * 
+     * @param cursoId id do Curso.
+     * @return Lista de Disciplinas.
      */
     @GetMapping("/curso/{cursoId}/disciplina")
     public List<Disciplina> getDisciplinasFromCurso(@PathVariable("cursoId") Long cursoId) {
         return cursoService.getDisciplinasFromCurso(cursoId);
     }
 
+    /**
+     * Insere uma Disciplina em um Curso específico.
+     * 
+     * @param cursoId id do Curso.
+     * @param disciplina Disciplina a ser inserida.
+     */
     @PostMapping("/curso/{cursoId}/disciplina")
     public void postDisciplinaInCurso(@PathVariable("cursoId") Long cursoId, @RequestBody Disciplina disciplina) {
         cursoService.saveDisciplinaInCurso(cursoId, disciplina);
     }
 
+    /**
+     * Modifica uma Disciplina de um Curso específico.
+     * 
+     * @param cId id do Curso.
+     * @param dId id da Disciplina.
+     * @param disciplina nova Disciplina contendo as informações a serem modificadas.
+     */
     @PutMapping("/curso/{cursoId}/disciplina/{disciplinaId}")
     public void putDisciplinaFromCurso(@PathVariable("cursoId") Long cId, @PathVariable("disciplinaId") Long dId, @RequestBody Disciplina disciplina) {
         cursoService.modifyDisciplinaFromCurso(cId, dId, disciplina);
     }
 
+    /**
+     * Deleta uma Disciplina de um Curso específico.
+     * 
+     * @param cId id do Curso.
+     * @param dId id da Disciplina.
+     */
     @DeleteMapping("/curso/{cursoId}/disciplina/{disciplinaId}")
     public void deleteDisciplinaFromCurso(@PathVariable("cursoId") Long cId, @PathVariable("disciplinaId") Long dId) {
         cursoService.deleteDisciplinaFromCurso(cId, dId);
     }
 
-    // Funcao para adicionar uma lista de disciplinas em um curso especifico
+    /**
+     * Insere uma lista de Disciplinas em um Curso.  
+     * Função muito útil para a inserção mais rápida das disciplinas de um curso.
+     * 
+     * @param cursoId id do Curso.
+     * @param disciplinas lista de Disciplinas a ser inserida.
+     */
     @PostMapping("/curso/{cursoId}/disciplinas")
     public void postDisciplinaListInCurso(@PathVariable("cursoId") Long cursoId, @RequestBody List<Disciplina> disciplinas) {
         cursoService.saveDisciplinaListInCurso(cursoId, disciplinas);
     }
 
-    /*
-     *  Operações HTTP para adicionar um pré requisito a uma disciplina
+    /**
+     * Insere um pré-requisito em um Curso específico.
+     * 
+     * @param cursoId id do Curso.
+     * @param dependencia indica qual Disciplina é pré-requisito da outra.
      */
     @PostMapping("/curso/{cursoId}/preRequisito")
     public void postDependencia(@PathVariable("cursoId") Long cursoId, @RequestBody Dependencia dependencia) {
         cursoService.addDependencia(cursoId, dependencia.getDisciplinaA(), dependencia.getDisciplinaB());
     }
 
-    // Funcao para adicionar uma lista de arestas em um curso especifico
+    /**
+     * Insere uma lista de pré-requisitos em um Curso.  
+     * Função muito útil para a inserção mais rápida dos pré-requisitos de um curso.
+     * 
+     * @param cursoId id do Curso.
+     * @param dependencias lista de dependencias.
+     */
     @PostMapping("curso/{cursoId}/preRequisitos")
     public void postDependenciaListInCurso(@PathVariable("cursoId") Long cursoId, @RequestBody List<Dependencia> dependencias) {
         cursoService.addDependenciaListInCurso(cursoId, dependencias);
     }
 
-    /*
-     *  Operações HTTP para retornar o caminho ao endpoint
+    /**
+     * Acha todas as disciplinas que podem ser encontradas a partir de dois dfs's (no grafo normal e transposto) 
+     * com aquela disciplina sendo o ponto inicial.
+     * 
+     * @param cId id do Curso.
+     * @param dId id da Disciplina.
+     * @return conjunto de pares de disciplinas. Indicam todos os caminhos pelos quais os dfs's andaram.
      */
     @GetMapping("curso/{cursoId}/path/{disciplinaId}")
     public Set<Pair<Disciplina, Disciplina>> getPathFromDisciplina(@PathVariable("cursoId") Long cId, @PathVariable("disciplinaId") Long dId) {
