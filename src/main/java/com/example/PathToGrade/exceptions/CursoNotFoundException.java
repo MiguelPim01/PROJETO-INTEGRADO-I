5740a47baa888107c96b19d157ce11b42cb889c2
MiguelPim01 <miguelmip02@gmail.com>
2024-07-03 00:38:00 -0300

    feat: Exceção para curso não encontrado adicionada

---
+++ src/main/java/com/example/PathToGrade/exceptions/CursoNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.PathToGrade.exceptions;
+
+public class CursoNotFoundException extends Exception {
+    
+    public CursoNotFoundException(String message) {
+        super(message);
+    }
+}
