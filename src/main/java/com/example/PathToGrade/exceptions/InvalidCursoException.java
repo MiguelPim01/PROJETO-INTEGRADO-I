4d675e66ba44b6093c2adbf58a5724f56fdd52d2
MiguelPim01 <miguelmip02@gmail.com>
2024-07-02 21:39:40 -0300

    feat: Exceção adicionada para Curso invalido

---
+++ src/main/java/com/example/PathToGrade/exceptions/InvalidCursoException.java
@@ -0,0 +1,9 @@
+package com.example.PathToGrade.exceptions;
+
+public class InvalidCursoException extends Exception {
+    
+    public InvalidCursoException(String message) {
+        super(message);
+    }
+    
+}
