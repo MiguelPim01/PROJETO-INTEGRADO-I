e020976369ee3ce703f4be6c9e9a3a220e41f2d3
MiguelPim01 <miguelmip02@gmail.com>
2024-07-03 18:06:47 -0300

    delete: Exceção CursoNotFoundException desnecessária

--- src/main/java/com/example/PathToGrade/exceptions/CursoNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.PathToGrade.exceptions;
-
-public class CursoNotFoundException extends Exception {
-    
-    public CursoNotFoundException(String message) {
-        super(message);
-    }
-}
