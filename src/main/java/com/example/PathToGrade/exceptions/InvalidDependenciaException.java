8f7f9443ce8ef5761bb419f21c238ff0e5b962bf
MiguelPim01 <miguelmip02@gmail.com>
2024-07-08 15:02:25 -0300

    docs: Documentação das exceções adicionada

--- src/main/java/com/example/PathToGrade/exceptions/InvalidDependenciaException.java
+++ src/main/java/com/example/PathToGrade/exceptions/InvalidDependenciaException.java
@@ -1,7 +1,20 @@
 package com.example.PathToGrade.exceptions;
 
+/**
+ * <i>Documentação da classe InvalidDependenciaException</i>
+ * 
+ * @author Miguel Vieira Machado Pim
+ * @see com.example.PathToGrade.exceptions.InvalidDependenciaException
+ * 
+ * Exceção usada quando se verifica que uma Dependencia é inválida.
+ */
 public class InvalidDependenciaException extends Exception {
     
+    /**
+     * Construtor.
+     * 
+     * @param message mensagem de erro.
+     */
     public InvalidDependenciaException(String message) {
         super(message);
     }
